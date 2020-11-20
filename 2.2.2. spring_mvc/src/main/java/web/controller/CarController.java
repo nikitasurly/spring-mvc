d8eb2c1f3c7b3f478d63f043f6ff93139700693a
nikita <mikropyl28@gmail.com>
2020-11-21 00:29:11 +0300

    Initial commit

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -18,7 +18,7 @@ public class CarController {
                            ModelMap model) {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("model1", 1, 2001));
-        cars.add(new Car("model2", 2, 2002));
+        cars.add(new Car("модель2", 2, 2002));
         cars.add(new Car("model3", 3, 2003));
         cars.add(new Car("model4", 4, 2004));
         cars.add(new Car("model5", 5, 2005));
